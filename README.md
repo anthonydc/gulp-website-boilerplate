b397c291294db2bcf82719ce72fdf491fe2f4e4c
Anthony Della Camera <anthonyd@Anthonys-MacBook-Pro-2.local>
2015-06-17 11:53:12 -0400

    revised entire build process

--- README.md
+++ README.md
@@ -1,10 +1,45 @@
-## gulp-website-boilerplate
+For all of this magic to work, your computer needs a few extra files to get Gulp up and running in your project.
 
-## you need Gulp
+You'll need to make sure you have both [Node](http://nodejs.org) and [Gulp](http://gulpjs.com) installed before moving forward. If you already have both, keep on going, and if not, you'll only need to install them once.
 
-[Get it here](http://gulpjs.com/)
+Navigate to your project directory using the command line and run the following command:
 
-## run these commands to get the ball rolling
+    npm install
 
-- npm install
-- gulp
+That's it! You're ready to start using Gulp.
+
+Using Gulp
+
+==========
+
+Default Task
+
+------------
+
+    gulp
+
+Running the default task automatically watches your project folders for any changes and runs the accompanying task. For example, if you've elected to run tasks on your JavaScript, anytime you change a JavaScript file gulp will automatically run those tasks, including a browser refresh if you've included BrowserSync.
+
+CSS
+
+---
+
+    gulp styles
+
+Running the gulp styles task will run your selected CSS tasks once.
+
+JavaScript
+
+----------
+
+    gulp scripts
+
+Running the gulp scripts task will run your selected JavaScript tasks once.
+
+Images
+
+------
+
+    gulp images
+
+Running the gulp images task will run your selected image tasks once.
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,69 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
-    gulp.src('scss/styles.scss')
-        .pipe(sass({includePaths: ['scss']}))
-        .pipe(gulp.dest('css'));
-});
-
 gulp.task('browser-sync', function() {
-    browserSync.init(["css/*.css", "js/*.js", "*.html"], {
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('images', function(){
+  gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/styles/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("src/styles/**/*.scss", ['styles']);
+  gulp.watch("src/scripts/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
 });
